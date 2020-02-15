a25efa90f56f124f5aefed583c23efd108a58e03
arcadio2 <arcadiolg2@gmail.com>
2020-02-15 11:18:59 -0600

    mi primer commit|

---
+++ code.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 15 10:51:45 2020
+
+@author: arcad
+"""
+
+#git add a staying area
+#git commit en la nube
+#git log --oneline
+#git_show commit_ID
+#si se quiere pasar diregcamente git commit -a-m"message
+print("Muere facebook")
